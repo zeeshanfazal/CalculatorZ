213d2ff054d2c8c5c06a2724cc304eb37a20dde4
zeeshan fazal <95215257+zeeshanfazal@users.noreply.github.com>
2022-07-18 14:15:10 +0530

    yes successful

---
+++ index.js
@@ -0,0 +1,23 @@
+let string = "";
+let buttons = document.querySelectorAll('.btn');
+Array.from(buttons).forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == 'C' || e.target.innerHTML == 'AC') {
+            string = "";
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == '← ') {
+            string = string.slice(0, string.length-1);
+            document.querySelector('input').value = string;
+        }
+    else {
+                console.log(e.target)
+                string = string + e.target.innerHTML;
+                document.querySelector('input').value = string;
+            }
+        })
+})
